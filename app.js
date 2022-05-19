fc57271df80b5db17db16ae57fe7246c11943c68
Gile <mladen.reljic87@gmail.com>
2022-05-19 16:57:45 +0200

    Add JS code that displays albums based on the value of the selector

--- app.js
+++ app.js
@@ -147,16 +147,84 @@ const discography = [
 	},
 	{
 		"id": 25,
-		"album": "Goretrance X ",
+		"album": "Goretrance X",
 		"year": "2017",
 		"cover": "img/goretrancex.jpg"
-	}
+	},
+	{
+		"id": 26,
+		"album": "Memestreet",
+		"year": "2017",
+		"cover": "img/meme.jpg"
+	},
+	{
+		"id": 27,
+		"album": "nrrv6",
+		"year": "2018",
+		"cover": "img/nrrv6.jpg"
+	},
+	{
+		"id": 28,
+		"album": "nineties rave retrospective collection 1",
+		"year": "2018",
+		"cover": "img/nrrc1.jpg"
+	},
+	{
+		"id": 29,
+		"album": "loather",
+		"year": "2018",
+		"cover": "img/loather.jpg"
+	},
+	{
+		"id": 30,
+		"album": "tower block knife crime ",
+		"year": "2019",
+		"cover": "img/tbkc.jpg"
+	},
+	{
+		"id": 31,
+		"album": "AP1 - Nineties Rave Retrospective Volume 7",
+		"year": "2019",
+		"cover": "img/nrrv7.jpg"
+	},
+	{
+		"id": 32,
+		"album": "i can't help you.",
+		"year": "2020",
+		"cover": "img/canthelp.jpg"
+	},
+	{
+		"id": 33,
+		"album": "splitshit 2",
+		"year": "2020",
+		"cover": "img/splitshit2.jpg"
+	},
+	{
+		"id": 343,
+		"album": "ap 2",
+		"year": "2021",
+		"cover": "img/ap2.jpg"
+	},
+	{
+		"id": 35,
+		"album": "Misdirection (With Hitori Tori)",
+		"year": "2021",
+		"cover": "img/misdirection.jpg"
+	},
 ]
 
 const albums = document.querySelector('.albums-container');
+const albumSelector = document.querySelector('#album-selector');
+let albumYear = [];
+
 
 window.addEventListener('DOMContentLoaded', () => {
 	showAlbums(discography);
+});
+
+albumSelector.addEventListener('change', e => {
+	let year = albumSelector.options[albumSelector.selectedIndex].value
+	filterDisc(year, discography);
 });
 
 let showAlbums = discography => {
@@ -172,8 +240,20 @@ let showAlbums = discography => {
 				<p class="item-text">more information</p>
 			</div>
 		</article>`;
-
 	});
 	albums.innerHTML = showDiscography.join('')
 };
 
+const filterDisc = (year, discography) => {
+	const filteredYear = discography.filter(selectedYear => {
+		if(selectedYear.year === year) {
+			console.log(selectedYear);
+			return selectedYear;
+		}
+	});
+	if(year === "all") {
+		showAlbums(discography);
+	} else {
+		showAlbums(filteredYear);
+	}
+};
