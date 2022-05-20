2f378d8d07cefd0130c3f123e00a7aaeefbe5173
Gile <mladen.reljic87@gmail.com>
2022-05-20 10:00:20 +0200

    Add all the albums

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
 const discography = [
 	{
 		"id": 1,
-		"album": "Where Is Soda Pop And Apple Sweets?",
-		"year": "2008",
-		"cover": "img/where.jpg"
+		"album": "watashi wa baka yaro desu",
+		"year": "2007",
+		"cover": "img/wwbyd.jpg"
 	},
 	{
 		"id": 2,
@@ -15,198 +15,360 @@ const discography = [
 	},
 	{
 		"id": 3,
+		"album": "Where Is Soda Pop And Apple Sweets?",
+		"year": "2008",
+		"cover": "img/where.jpg"
+	},
+	{
+		"id": 4,
+		"album": "old shit wot never got released but not kind of has in this nifty little zip file",
+		"year": "2008",
+		"cover": "img/osht.jpg"
+	},
+	{
+		"id": 5,
+		"album": "otakutone mega high speed remix party album",
+		"year": "2009",
+		"cover": "img/otktn.jpg"
+	},
+	{
+		"id": 6,
 		"album": "My Love Feels All Wrong",
 		"year": "2009",
 		"cover": "img/mylove.jpg"
 	},
 	{
-		"id": 4,
+		"id": 7,
+		"album": "the incubus mark/pulse of awakening",
+		"year": "2009",
+		"cover": "img/incubus.jpg"
+	},
+	{
+		"id": 8,
+		"album": "with all my heart.",
+		"year": "2010",
+		"cover": "img/withall.jpg"
+	},
+	{
+		"id": 9,
+		"album": "ureshii!",
+		"year": "2010",
+		"cover": "img/ureshii.jpg"
+	},
+	{
+		"id": 10,
 		"album": "Mainstream Muthafucka!",
 		"year": "2010",
 		"cover": "img/mainstream.jpg"
 	},
 	{
-		"id": 5,
-		"album": "Steppa",
-		"year": "2010",
-		"cover": "img/steppa.jpg"
-	},
-	{
-		"id": 6,
+		"id": 11,
 		"album": "Dancefloor Degrader",
 		"year": "2010",
 		"cover": "img/dancefloor.jpg"
 	},
 	{
-		"id": 7,
+		"id": 12,
+		"album": "Steppa",
+		"year": "2010",
+		"cover": "img/steppa.jpg"
+	},
+	{
+		"id": 13,
+		"album": "paranoia ep",
+		"year": "2010",
+		"cover": "img/pep.jpg"
+	},
+	{
+		"id": 14,
 		"album": "Overspecialized",
 		"year": "2011",
 		"cover": "img/oversp.jpg"
 	},
 	{
-		"id": 8,
+		"id": 15,
+		"album": "semantic compositions on death and its meaning",
+		"year": "2011",
+		"cover": "img/semantic.jpg"
+	},
+	{
+		"id": 16,
+		"album": "nineties rave retrospective volume 1",
+		"year": "2011",
+		"cover": "img/nnrv1.jpg"
+	},
+	{
+		"id": 17,
+		"album": "gnb",
+		"year": "2011",
+		"cover": "img/gnb.jpg"
+	},
+	{
+		"id": 18,
 		"album": "Blue Girl On Sunday",
 		"year": "2011",
 		"cover": "img/blue.jpg"
 	},
 	{
-		"id": 9,
+		"id": 19,
 		"album": "Bleak",
 		"year": "2011",
 		"cover": "img/bleak.jpg"
 	},
 	{
-		"id": 10,
+		"id": 20,
+		"album": "defective beats (rough cuts)",
+		"year": "2011",
+		"cover": "img/rough.jpg"
+	},
+	{
+		"id": 21,
+		"album": "Splitshit",
+		"year": "2012",
+		"cover": "img/splitshit.jpg"
+	},
+	{
+		"id": 22,
+		"album": "dissection",
+		"year": "2012",
+		"cover": "img/diss.jpg"
+	},
+	{
+		"id": 23,
 		"album": "Wake Up, Painful Sister!",
 		"year": "2012",
 		"cover": "img/wakeup.jpg"
 	},
 	{
-		"id": 11,
+		"id": 24,
+		"album": "nineties rave retrospective volume 2",
+		"year": "2012",
+		"cover": "img/nrrv2.jpg"
+	},
+	{
+		"id": 25,
 		"album": "Ministry Of Shit",
 		"year": "2013",
 		"cover": "img/ministry.jpg"
 	},
 	{
-		"id": 12,
+		"id": 26,
 		"album": "Rituals",
 		"year": "2013",
 		"cover": "img/rituals.jpg"
 	},
 	{
-		"id": 13,
-		"album": "Wish I Was Here (or How Things Went Wrong) ",
-		"year": "2014",
-		"cover": "img/wish.jpg"
+		"id": 27,
+		"album": "lorazepam resonance ep",
+		"year": "2013",
+		"cover": "img/lora.jpg"
 	},
 	{
-		"id": 14,
-		"album": "Mlsfaw",
-		"year": "2014",
-		"cover": "img/mlsfaw.jpg"
-	},
-	{
-		"id": 15,
+		"id": 28,
 		"album": "end.",
 		"year": "2014",
 		"cover": "img/end.jpg"
 	},
 	{
-		"id": 16,
-		"album": "Goretrance 9",
-		"year": "2015",
-		"cover": "img/goretrance9.jpg"
+		"id": 29,
+		"album": "copyright infringement ep",
+		"year": "2014",
+		"cover": "img/copy.jpg"
 	},
 	{
-		"id": 17,
-		"album": "With All My Heart ",
-		"year": "2015",
-		"cover": "img/myheart.jpg"
+		"id": 30,
+		"album": "Wish I Was Here (or How Things Went Wrong) ",
+		"year": "2014",
+		"cover": "img/wish.jpg"
 	},
 	{
-		"id": 18,
+		"id": 31,
+		"album": "nrrv3-0.37a",
+		"year": "2014",
+		"cover": "img/nrrv3.jpg"
+	},
+	{
+		"id": 32,
+		"album": "Mlsfaw",
+		"year": "2014",
+		"cover": "img/mlsfaw.jpg"
+	},
+	{
+		"id": 33,
+		"album": "goreshit & yeongrak - safety cuts",
+		"year": "2014",
+		"cover": "img/gsyeon.jpg"
+	},
+	{
+		"id": 34,
 		"album": "Semantics: The Benzo Chronicles",
 		"year": "2015",
 		"cover": "img/semantics.jpg"
 	},
 	{
-		"id": 19,
-		"album": "I Hardly Knew You",
-		"year": "2016",
-		"cover": "img/hardly.jpg"
+		"id": 35,
+		"album": "My Love Feels All Wrong digipak",
+		"year": "2015",
+		"cover": "img/mylove.jpg"
 	},
 	{
-		"id": 20,
-		"album": "Sedatives Vol. 1",
-		"year": "2016",
-		"cover": "img/sedatives1.jpg"
+		"id": 36,
+		"album": "Tomboyish Love For Daughter digipak",
+		"year": "2015",
+		"cover": "img/tomboyishd.jpg"
 	},
 	{
-		"id": 21,
-		"album": "Sedatives Vol. 2",
-		"year": "2016",
-		"cover": "img/sedatives2.jpg"
+		"id": 37,
+		"album": "semantics digipak",
+		"year": "2015",
+		"cover": "img/semdigi.jpg"
 	},
 	{
-		"id": 22,
-		"album": "Sedatives Vol. 3",
-		"year": "2016",
-		"cover": "img/sedatives3.jpg"
+		"id": 38,
+		"album": "Goretrance 9",
+		"year": "2015",
+		"cover": "img/goretrance9.jpg"
 	},
 	{
-		"id": 23,
+		"id": 39,
+		"album": "nrrv4",
+		"year": "2015",
+		"cover": "img/nrrv4.jpg"
+	},
+	{
+		"id": 40,
+		"album": "with all my heart. digipak",
+		"year": "2015",
+		"cover": "img/myheart.jpg"
+	},
+	{
+		"id": 41,
+		"album": "dancefloor muthafucka! digipak",
+		"year": "2015",
+		"cover": "img/dancefmf.jpg"
+	},
+	{
+		"id": 42,
+		"album": "goreshit/Ayane Fukumi - loli or die",
+		"year": "2015",
+		"cover": "img/lod.jpg"
+	},
+	{
+		"id": 43,
 		"album": "You Get The Tracks You Deserve!",
 		"year": "2016",
 		"cover": "img/youget.jpg"
 	},
 	{
-		"id": 24,
+		"id": 44,
+		"album": "Sedatives Vol. 1",
+		"year": "2016",
+		"cover": "img/sedatives1.jpg"
+	},
+	{
+		"id": 45,
+		"album": "Sedatives Vol. 2",
+		"year": "2016",
+		"cover": "img/sedatives2.jpg"
+	},
+	{
+		"id": 46,
+		"album": "I Hardly Knew You",
+		"year": "2016",
+		"cover": "img/hardly.jpg"
+	},
+	{
+		"id": 47,
+		"album": "Sedatives Vol. 3",
+		"year": "2016",
+		"cover": "img/sedatives3.jpg"
+	},
+	{
+		"id": 48,
+		"album": "blue girl on sunday​/​wake up, painful sister! digipak",
+		"year": "2016",
+		"cover": "img/bgs.jpg"
+	},
+	{
+		"id": 49,
+		"album": "nrrv5",
+		"year": "2016",
+		"cover": "img/nrrv5.jpg"
+	},
+	{
+		"id": 50,
 		"album": "Stankface",
 		"year": "2017",
 		"cover": "img/stankface.jpg"
 	},
 	{
-		"id": 25,
+		"id": 51,
+		"album": "a decade of shit - the very most bearable of goreshit",
+		"year": "2017",
+		"cover": "img/ados.jpg"
+	},
+	{
+		"id": 52,
 		"album": "Goretrance X",
 		"year": "2017",
 		"cover": "img/goretrancex.jpg"
 	},
 	{
-		"id": 26,
+		"id": 53,
 		"album": "Memestreet",
 		"year": "2017",
 		"cover": "img/meme.jpg"
 	},
 	{
-		"id": 27,
+		"id": 54,
 		"album": "nrrv6",
 		"year": "2018",
 		"cover": "img/nrrv6.jpg"
 	},
 	{
-		"id": 28,
+		"id": 55,
 		"album": "nineties rave retrospective collection 1",
 		"year": "2018",
 		"cover": "img/nrrc1.jpg"
 	},
 	{
-		"id": 29,
+		"id": 56,
 		"album": "loather",
 		"year": "2018",
 		"cover": "img/loather.jpg"
 	},
 	{
-		"id": 30,
+		"id": 57,
 		"album": "tower block knife crime ",
 		"year": "2019",
 		"cover": "img/tbkc.jpg"
 	},
 	{
-		"id": 31,
+		"id": 58,
 		"album": "AP1 - Nineties Rave Retrospective Volume 7",
 		"year": "2019",
 		"cover": "img/nrrv7.jpg"
 	},
 	{
-		"id": 32,
+		"id": 59,
 		"album": "i can't help you.",
 		"year": "2020",
 		"cover": "img/canthelp.jpg"
 	},
 	{
-		"id": 33,
+		"id": 60,
 		"album": "splitshit 2",
 		"year": "2020",
 		"cover": "img/splitshit2.jpg"
 	},
 	{
-		"id": 343,
+		"id": 61,
 		"album": "ap 2",
 		"year": "2021",
 		"cover": "img/ap2.jpg"
 	},
 	{
-		"id": 35,
+		"id": 62,
 		"album": "Misdirection (With Hitori Tori)",
 		"year": "2021",
 		"cover": "img/misdirection.jpg"
